f6ead6f6f6d13f34c82d0968481b8f064abe820a
ratnakumar_b <ratnakumar_b@persistent.com>
2019-08-02 16:11:48 +0530

    push test to prod

--- insertgmrtdata.py
+++ insertgmrtdata.py
@@ -26,22 +26,22 @@ class InsertGmrtDataController:
                 file_path = os.path.dirname(each_lta)
                 proj_code = gmrt_db_utils.get_projcode_by_ltaname(lta_file)
                 if proj_code != 0:
-                    proj_code_with_date = os.path.basename(file_path)
-                    if '_' in proj_code_with_date:
-                        proj_code = proj_code_with_date[:6]
-                    if ("DDT" in proj_code_with_date or "ddt" in proj_code_with_date):
-                        proj_code = proj_code_with_date[:7].replace('DDT','ddt') 
-                    backend_type = gmrt_db_utils.get_backend_type(proj_code)
-                    print("==> ",proj_code, " === ", backend_type)
-                    if backend_type != 0:
-                        backend_type= backend_type[0]
-                        if ("GWB" in backend_type or "gwb" in backend_type):
-                            if ("GWB" in each_lta or "gwb" in each_lta):
-                                print(backend_type, "=== GWB")
-                                to_db_list.append(each_lta)
-                        else:    
-                            print(backend_type, "=== GSB")
-                            to_db_list.append(each_lta)
+                    # proj_code_with_date = os.path.basename(file_path)
+                    # if '_' in proj_code_with_date:
+                    #     proj_code = proj_code_with_date[:6]
+                    # if ("DDT" in proj_code_with_date or "ddt" in proj_code_with_date):
+                    #     proj_code = proj_code_with_date[:7].replace('DDT','ddt') 
+                    # backend_type = gmrt_db_utils.get_backend_type(proj_code)
+                    # print("==> ",proj_code, " === ", backend_type)
+                    # if backend_type != 0:
+                    #     backend_type= backend_type[0]
+                    #     if ("GWB" in backend_type or "gwb" in backend_type):
+                    #         if ("GWB" in each_lta or "gwb" in each_lta):
+                    #             print(backend_type, "=== GWB")
+                    #             to_db_list.append(each_lta)
+                    # else:    
+                    #     print(backend_type, "=== GSB")
+                    to_db_list.append(each_lta)
                             
         print("----------")
         print(to_db_list)
